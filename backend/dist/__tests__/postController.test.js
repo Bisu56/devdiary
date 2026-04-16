0a6a4d75f9a43fd8becd1c706fd8350aa5344fa6
Bisu56 <rajbanshibishal441@gmail.com>
2026-04-16 16:14:35 +0545

    ui polishing

--- backend/dist/__tests__/postController.test.js
+++ backend/dist/__tests__/postController.test.js
@@ -1,20 +1,6 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 describe('Post Controller', () => {
-    let mockRequest;
-    let mockResponse;
-    let jsonMock;
-    let statusMock;
-    beforeEach(() => {
-        jsonMock = jest.fn();
-        statusMock = jest.fn().mockReturnValue({ json: jsonMock });
-        mockRequest = {};
-        mockResponse = {
-            json: jsonMock,
-            status: statusMock,
-        };
-    });
-    it('should be defined', () => {
+    it('should pass basic test', () => {
         expect(true).toBe(true);
     });
 });
